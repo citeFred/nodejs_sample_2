ed2f5566e31414ba99af3a0395c48c27cea0d90e
citeFred <citefred@gmail.com>
2025-01-09 11:36:56 +0900

    인터페이스와 타입별칭의 확장

--- src/basic/basic.ts
+++ src/basic/basic.ts
@@ -31,18 +31,18 @@ function flipCoin() {
 let x = 3;
 // x = [0, 1, null];
 
-class Animal {}
-class Rhino extends Animal {
-    hasHorn: boolean = true;
-}
-class Elephant extends Animal {
-    hasTrunk: boolean = true;
-}
-class Snake extends Animal {
-    hasLegs: boolean = false;
-}
+// class Animal {}
+// class Rhino extends Animal {
+//     hasHorn: boolean = true;
+// }
+// class Elephant extends Animal {
+//     hasTrunk: boolean = true;
+// }
+// class Snake extends Animal {
+//     hasLegs: boolean = false;
+// }
 
-let zoo = [new Rhino(), new Elephant(), new Snake()];
+// let zoo = [new Rhino(), new Elephant(), new Snake()];
 // Animal[] 추론 추측, 그러나 다른 타입이 지정됨.
 
 
