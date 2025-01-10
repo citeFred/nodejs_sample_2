0c6cd2a6c4754690088a54177b84ca9c359af0d2
citeFred <citefred@gmail.com>
2025-01-10 15:21:47 +0900

    타입스크립트의 모듈 export/import

---
+++ src/modules/module.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var mathOperatios_1 = require("./mathOperatios");
+console.log((0, mathOperatios_1.add)(1, 5));
+console.log((0, mathOperatios_1.substract)(1, 5));
+console.log((0, mathOperatios_1.multiply)(1, 5));
+console.log((0, mathOperatios_1.divide)(1, 5));
